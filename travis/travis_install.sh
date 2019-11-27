6623982e5127668f1bd3efbac4a3a50637e8b224
Thomas Barnet-Lamb <tbl@no.spam.please>
2019-11-27 17:54:30 -0500

    correct cabal install command

--- travis/travis_install.sh
+++ travis/travis_install.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
+if [[ $(cabal --numeric-version |  cut -d. -f1) -le 2 ]]
+then
+    CABAL_INSTALL_COMMAND=install
+else
+    CABAL_INSTALL_COMMAND=v1-install
+fi
+
 if [[ "$BUILD_TYPE" == "dynamic" ]]
 then
   cabal --version
-  cabal v1-install --reinstall --force-reinstalls --enable-shared text transformers mtl parsec hashable unordered-containers ghc-paths
+  cabal $CABAL_INSTALL_COMMAND --reinstall --force-reinstalls --enable-shared text transformers mtl parsec hashable unordered-containers ghc-paths
 fi
 
 if [[ "$BUILD_TYPE" == "static" ]]
 then
   cabal --version
-  cabal v1-install text transformers mtl parsec hashable unordered-containers ghc-paths
+  cabal $CABAL_INSTALL_COMMAND text transformers mtl parsec hashable unordered-containers ghc-paths
 fi
